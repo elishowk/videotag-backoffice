696215db01bdf64b8e26e5392dd83ad69f6f176b
matthieuHanne <matthieu.hanne@commonecoute.fr>
2012-12-20 11:40:08 +0100

    initial commit

---
+++ index.tpl
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8" />
+    <title>BackOffice</title>
+  </head>
+  <body>
+    <script>
+      (function () {
+       'use strict';
+
+       /**
+         * retrieve config
+         */
+        var config = {
+            'baseUrl': {STATIC_URL},
+            'configUrl': {CONFIG_URL},
+        };
+
+        /**
+         * requirejs
+         */
+        var script = document.createElement('script');
+        script.addEventListener('load', function () {
+          require.appConfig = config;
+        });
+        script.setAttribute('src', config.baseUrl + '/lib/require-2.1.2.min.js');
+        script.setAttribute('data-main', config.baseUrl + '/videotag-backoffice/main.js');
+        document.head.appendChild(script);
+      }).call(window);
+    </script>
+  </body>
+</html>
---
+++ main.js
@@ -0,0 +1,40 @@
+/*global define*/
+
+require.config({
+    'baseUrl': require.appConfig.baseUrl,
+    'paths': {
+        'backbone': 'lib/backbone-0.9.9.min',
+        'domready': 'lib/require-domready-2.0.1.min',
+        'text':     'lib/require-text-2.0.3.min',
+        'i18n':     'lib/require-i18n-2.0.1.min',
+        'jquery':   'lib/jquery-1.8.3.min',
+        'underscore':   'lib/lodash-1.0.0.min',
+        'spin': 'lib/spin-1.2.7.js',
+        'backbone-validation': 'lib/backbone-validation-0.6.2.js',
+        'bootstrap': 'lib/bootstrap-2.2.2',
+        'bootstrap-datepicker': 'lib/bootstrap-datepicker-2.0.js',
+        'bootstrap-timepicker': 'lib/bootstrap-timepicker-2.0.js',
+        'jquery-gravatar': 'lib/jquery-gravatar-1.0.1.js',
+        'md5': 'lib/md5-2.1.js',
+        'raven': 'lib/raven-0.6.min.js',
+        'app':      'videotag-backoffice/app',
+        'modules':  'videotag-widget/modules'
+    },
+    'shim': {
+        'app': {
+            'deps': ['backbone']
+        },
+        'backbone': {
+            'deps': [
+                'underscore',
+                'jquery'
+            ],
+            'exports': 'Backbone'
+        }
+    }
+});
+
+define(['app', 'poser/backbone-tastypie-0.1'], function (App) {
+    App.initialize();
+});
+
